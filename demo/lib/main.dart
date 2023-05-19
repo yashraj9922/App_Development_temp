78a71624e874221062dc3fb49ab2be77a9dd11e8
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-05-19 16:00:09 +0530

    image added to app

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title});
 
   final String title;
 
@@ -32,35 +32,51 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text('Demo Testing App')),
-        ),
-        body:
-            Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
+      appBar: AppBar(
+        title: const Center(child: Text('Demo Testing App')),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              child: Image.asset('assets/images/abc.png'),
+            ),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
                     width: 90,
                     height: 90,
                     color: Colors.blueGrey,
-                    child: const Center(child: Text(
-                      'Yashraj Kadam',
-                      style: TextStyle(
-                        color: Colors.limeAccent,
-                        fontSize: 10,
-                        fontWeight: FontWeight.bold,
-                        backgroundColor: Colors.black)),),),
-            TextButton(
-          child: Text('Click here'),
-          onPressed: () {
-            print('Text Button Clicked');
-          },
-          onLongPress: () {
-            print('Longpressed done');
-          },
-          ),
-        ],
+                    child: const Center(
+                      child: Text(
+                        'Yashraj Kadam',
+                        style: TextStyle(
+                          color: Colors.limeAccent,
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold,
+                          backgroundColor: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    child: Text('Click here'),
+                    onPressed: () {
+                      print('Text Button Clicked');
+                    },
+                    onLongPress: () {
+                      print('Longpressed done');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
