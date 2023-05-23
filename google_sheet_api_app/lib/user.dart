e3b7237b623e786988d047c4eda7cee8080f14f3
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-05-24 03:04:24 +0530

    remove app

--- google_sheet_api_app/lib/user.dart
+++
@@ -1,9 +0,0 @@
-class UserFields {
-  //these are the title(rows) which we need to have in our spreadsheet(of a particular worksheet)
-  static final String id = 'id';
-  static final String name = 'name';
-  static final String email = 'email';
-  static final String isBeginner = 'isBeginner';
-
-  static List<String> getFields() => [id, name, email, isBeginner]; //we create a lists of string using getFields method
-}
