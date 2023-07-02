44b56f540b56e5bbb9482853270601f78471d1a3
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-07-02 12:26:55 +0530

    final commit

--- demo01/lib/main.dart
+++ demo01/lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(DemoApp1());
-}
+// void main() {
+//   runApp(DemoApp1());
+// }
+
+void main() => runApp(DemoApp1());
 
 class DemoApp1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    return const MaterialApp(
+      home: Text('Hello!'),
+    );
   }
 }
