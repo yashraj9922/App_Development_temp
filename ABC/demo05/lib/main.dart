51cebec36b82728a7965709b3fec1e47cf193c80
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-07-12 12:27:38 +0530

    .

--- ABC/demo05/lib/main.dart
+++ ABC/demo05/lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(Demo05());
+  runApp(const Demo05());
 }
 
 class Demo05 extends StatelessWidget {
+  const Demo05({super.key});
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(appBar: AppBar(
         title: const Text("My name is Yashraj Kadam"),
       ),
-      body: Center(
+      body: const Center(
         child: Text("Yashraj Kadam"),
       ),
       ),
